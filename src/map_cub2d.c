cc6e1a98bdc486e1e8d413d15393ea349b838407
Zhunya-bz <fluffylion2013@gmail.com>
2021-11-25 16:23:56 +0300

    norme4

--- src/map_cub2d.c
+++ src/map_cub2d.c
@@ -6,19 +6,19 @@
 /*   By: saltmer <saltmer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 12:27:14 by                   #+#    #+#             */
-/*   Updated: 2021/11/24 13:39:15 by                  ###   ########.fr       */
+/*   Updated: 2021/11/25 16:22:39 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../cub3D.h"
+#include "../cub3D.h"
 
 void	my_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = data->p_addres->addr + (y * data->p_addres->line_length + x *
-			(data->p_addres->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+	dst = data->p_addres->addr + (y * data->p_addres->line_length + x
+			* (data->p_addres->bits_per_pixel / 8));
+	*(unsigned int *) dst = color;
 }
 
 int	create_trgb(int t, int r, int g, int b)
@@ -26,16 +26,16 @@ int	create_trgb(int t, int r, int g, int b)
 	return (t << 24 | r << 16 | g << 8 | b);
 }
 
-static void ft_draw_rec(t_data *data, int start_i, int start_j, int color)
+static void	ft_draw_rec(t_data *data, int start_i, int start_j, int color)
 {
-	int k;
-	int l;
+	int	k;
+	int	l;
 
 	k = start_i;
-	while (k < SCALE + start_i) // по высоте-это у
+	while (k < SCALE + start_i)
 	{
 		l = start_j;
-		while (l < SCALE + start_j) // по ширине-это х
+		while (l < SCALE + start_j)
 		{
 			my_pixel_put(data, l, k, color);
 			l++;
@@ -44,28 +44,28 @@ static void ft_draw_rec(t_data *data, int start_i, int start_j, int color)
 	}
 }
 
-void ft_draw_map(t_data *data)
+void	ft_draw_map(t_data *data)
 {
-	int i;
-	int j;
+	int	i;
+	int	j;
 
 	i = 0;
-	ft_draw_rec(data, data->p_info->screen_height -
-					  data->p_info->height * SCALE + i * SCALE + (int)data->p_coord->y * SCALE, (int)data->p_coord->x *
-	SCALE,0xFF0000);
+	ft_draw_rec(data, data->p_info->screen_height
+		- data->p_info->height * SCALE + i * SCALE + (int)data->p_coord->y
+		* SCALE, (int)data->p_coord->x * SCALE, 0xFF0000);
 	while (i < data->p_info->height)
 	{
 		j = 0;
-		while(data->p_info->arr[i][j])
+		while (data->p_info->arr[i][j])
 		{
 			if (data->p_info->arr[i][j] == '1')
-				ft_draw_rec(data, data->p_info->screen_height -
-				data->p_info->height * SCALE + i *
-				SCALE, j * SCALE, 0x241a1a);
+				ft_draw_rec(data, data->p_info->screen_height
+					- data->p_info->height * SCALE + i
+					* SCALE, j * SCALE, 0x241a1a);
 			j++;
 		}
 		i++;
 	}
 	mlx_put_image_to_window(data->p_draw->mlx, data->p_draw->win,
-							data->p_addres->img, 0, 0);
+		data->p_addres->img, 0, 0);
 }
