309d521060c7040ddb91b342bd3b391b48b13164
Edwardo Richelle <erichell@ge-c6.kzn.21-school.ru>
2021-11-25 16:44:18 +0300

    vadim

--- src/move.c
+++ src/move.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   move.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By:  <saltmer>                                 +#+  +:+       +#+        */
+/*   By: erichell <erichell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 13:17:46 by                   #+#    #+#             */
-/*   Updated: 2021/11/25 13:31:19 by                  ###   ########.fr       */
+/*   Updated: 2021/11/25 16:42:54 by erichell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,16 @@ static void	arrow_press(t_data *data, double speed)
 	double	old_dir_x;
 	double	old_plane_x;
 
-	old_dir_x = data->p_coord->vecX;
-	data->p_coord->vecX = data->p_coord->vecX * cos(speed) - data->p_coord->vecY
-		* sin(speed);
-	data->p_coord->vecY = old_dir_x * sin(speed) + data->p_coord->vecY
+	old_dir_x = data->p_coord->vec_x;
+	data->p_coord->vec_x = data->p_coord->vec_x * \
+	cos(speed) - data->p_coord->vec_y * sin(speed);
+	data->p_coord->vec_y = old_dir_x * sin(speed) + data->p_coord->vec_y
 		* cos(speed);
-	old_plane_x = data->p_coord->planeX;
-	data->p_coord->planeX = data->p_coord->planeX * cos
-		(speed) - data->p_coord->planeY * sin(speed);
-	data->p_coord->planeY = old_plane_x * sin(speed)
-		+ data->p_coord->planeY * cos(speed);
+	old_plane_x = data->p_coord->plane_x;
+	data->p_coord->plane_x = data->p_coord->plane_x * cos
+		(speed) - data->p_coord->plane_y * sin(speed);
+	data->p_coord->plane_y = old_plane_x * sin(speed)
+		+ data->p_coord->plane_y * cos(speed);
 	ft_draw_3d(data);
 	ft_draw_map(data);
 }
@@ -42,11 +42,11 @@ static void	arrow_press(t_data *data, double speed)
 static void	press_wasd(t_data *data, double vectorX, double vectorY)
 {
 	if (data->p_info->arr[(int)(data->p_coord->y)][(int)
-			(data->p_coord->x + vectorX * data->p_coord->moveSpeed)] != '1')
-		data->p_coord->x += vectorX * data->p_coord->moveSpeed;
+			(data->p_coord->x + vectorX * data->p_coord->move_speed)] != '1')
+		data->p_coord->x += vectorX * data->p_coord->move_speed;
 	if (data->p_info->arr[(int)(data->p_coord->y + vectorY
-			* data->p_coord->moveSpeed)][(int)(data->p_coord->x)] != '1')
-		data->p_coord->y += vectorY * data->p_coord->moveSpeed;
+			* data->p_coord->move_speed)][(int)(data->p_coord->x)] != '1')
+		data->p_coord->y += vectorY * data->p_coord->move_speed;
 	ft_draw_3d(data);
 	ft_draw_map(data);
 }
@@ -59,16 +59,16 @@ int	key_press(int keycode, t_data *data)
 		close_win(data);
 	}
 	if (keycode == 13)
-		press_wasd(data, data->p_coord->vecX, data->p_coord->vecY);
+		press_wasd(data, data->p_coord->vec_x, data->p_coord->vec_y);
 	if (keycode == 1)
-		press_wasd(data, -data->p_coord->vecX, -data->p_coord->vecY);
+		press_wasd(data, -data->p_coord->vec_x, -data->p_coord->vec_y);
 	if (keycode == 123)
-		arrow_press(data, -data->p_coord->rotSpeed);
+		arrow_press(data, -data->p_coord->rot_speed);
 	if (keycode == 124)
-		arrow_press(data, data->p_coord->rotSpeed);
+		arrow_press(data, data->p_coord->rot_speed);
 	if (keycode == 2)
-		press_wasd(data, data->p_coord->planeX, data->p_coord->planeY);
+		press_wasd(data, data->p_coord->plane_x, data->p_coord->plane_y);
 	if (keycode == 0)
-		press_wasd(data, -data->p_coord->planeX, -data->p_coord->planeY);
+		press_wasd(data, -data->p_coord->plane_x, -data->p_coord->plane_y);
 	return (0);
 }
